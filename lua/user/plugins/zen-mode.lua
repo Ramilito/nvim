9f4e58d507b42ae1db4cd96399c9da77bc0ce2b7
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-01-07 12:49:08 +0100

    added key to zenmode

--- lua/user/plugins/zen-mode.lua
+++ lua/user/plugins/zen-mode.lua
@@ -1,11 +1,14 @@
 return {
-  "folke/zen-mode.nvim",
-  cmd = "ZenMode",
-  config = {
-    plugins = {
-      gitsigns = true,
-      tmux = true,
-      kitty = { enabled = false, font = "+2" },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    config = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
     },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
 }

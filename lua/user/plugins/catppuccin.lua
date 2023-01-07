0e2694657587a724f38e6f962c333db14a9bcc45
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-01-07 12:57:57 +0100

    fix theme

--- lua/user/plugins/catppuccin.lua
+++ lua/user/plugins/catppuccin.lua
@@ -1,7 +1,10 @@
 local M = {
   "catppuccin/nvim",
   lazy = false,
-  priority = 100,
+  priority = 1000,
+  config = function()
+    vim.cmd [[ colorscheme catppuccin ]]
+  end,
 }
 
 -- function M.config()
